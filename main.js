53a457ac2db1c21b12f0e707ae52f788f1a00c81
Burlington <perezuburlington@gmail.com>
2026-01-15 17:32:45 +0200

    hero section

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const navbarMobile = document.querySelector('.mobile');
 const navbarItems = document.querySelector('.nav-items');
-const navContact = document.querySelector('.nav-contact');
 
 navbarMobile.addEventListener('click', () => {
     navbarItems.classList.toggle('active');
-    navContact.classList.toggle('active');
 });
